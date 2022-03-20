94e0471530eedec354d3d768ae7e77e79c24c0c9
jasonketo <42648010+jasonketo@users.noreply.github.com>
2022-03-20 23:35:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,10 +18,10 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = '__ckguid=ciT3IO8CNUfItncc6XA8y82; __jsluid_s=7850dc6f395cdc2f7c42ea1f7c49c782; r_sort_type=score; device_id=24517578016430146797653790adf525d2427f7092c8ce0f7b8726390; DISABLE_APP_TIP=1; __jsluid_h=a0fff089b04433772ea50623e161410b; homepage_sug=c; _zdmA.vid=*; footer_floating_layer=0; ad_date=20; ad_json_feed=%7B%7D; trdipcktrffcext=1; sess=AT-mnfoeyYXQHacrboErf1Nxe1R4VM6nMXNmKkpupxGtfOmdq0VSEgzc5bzvPMWI4KJ3xPj4FapmtfgIMYkR7LLpA8xp%2FdAKK4msrOTS3Cv3U5coB1AKBmLn6U%3D; user=user%3A3754246540%7C3754246540; _zdmA.uid=ZDMA.srgtBpxKq.1647781171.2419200; smzdm_id=3754246540; bannerCounter=%5B%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%5D; _zdmA.time=1647781175775.3037.https%3A%2F%2Fwww.smzdm.com%2F; smzdm_user_view=D2F8D1F5313BCEBA9854EE58738FC443; smzdm_user_source=D2E7EC2EE87085247BCE58044DC6EE37; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%223754246540%22%2C%22first_id%22%3A%2217c6036f065318-08de0d4c70bc77-10473c18-3686400-17c6036f0668b8%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22%24device_id%22%3A%2217c6036f065318-08de0d4c70bc77-10473c18-3686400-17c6036f0668b8%22%7D'
 
 
 """
 调试用 SERVERCHAN_SECRETKEY
 """
-SERVERCHAN_SECRETKEY = ''
+SERVERCHAN_SECRETKEY = 'SCT93240Taq5lQYYYkjLzB4QbeWoL8LOd'
